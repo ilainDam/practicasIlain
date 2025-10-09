b60abecd58559963fc9e338c08a001ea20652290
ilain <jacoboperdon@gmail.com>
2025-10-09 20:02:53 +0100

    practicas 1-4 y 9

--- practicasTSX/src/Practica09.tsx
+++ practicasTSX/src/Practica09.tsx
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 
-class Practica09 extends Component{
+class Practica09 extends Component {
 
-state = {numero:1}
+  state = { numero: 1 }
 
-  render(){
-    const {numero} = this.state;
-    return(
-        <div>
-          Tabla del 2
-          <p>2*{numero} = {2*numero}</p>
-          <p>
-            <button onClick={()=>this.setState({numero: numero + 1})}>2*{numero+1}</button>
-          </p>
-        </div>
-      )
+  render() {
+    let { numero } = this.state;
+    {
+      if (numero == 11) {
+        numero = 1;
+      }
+    }
+    return (
+      <div>
+        Tabla del 2
+        <p>2*{numero} = {2 * numero}</p>
+        <p>
+          <button onClick={() => this.setState({ numero: numero + 1 })}>2*{numero + 1}</button>
+        </p>
+      </div>
+    )
   }
 }
 export default Practica09;
