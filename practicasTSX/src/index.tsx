1bd81a0c72d9822a3a54aa0d5b702188b1a82a49
ilain <jacoboperdon@gmail.com>
2025-10-20 14:48:12 +0100

    Practicas 1-15

--- practicasTSX/src/index.tsx
+++ practicasTSX/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Practica09 from './Practica09';
-import PracticaTestEjercicio from './PracticaTestEjercicio';
+import Practica09 from './practicasClase/practica9/Practica09';
+import PracticaTestEjercicio from './Extra/PracticaTestEjercicio';
 import EjemploApiLentaUseEffect from './Extra/EjemploApiLentaUseEffect';
 import Practica21 from './practicasClase/practica21/Practica21';
 
