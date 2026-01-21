973fa619c9f35c3adb0c2a2f6f0a1f4ecd7f8489
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 <jacoboperdon@gmail.com>
2026-01-21 16:53:38 +0000

    añadido checkbox a la lista de tareas

--- pruebaIlain/src/app/ActividadListaTarea/ListaTarea.tsx
+++ pruebaIlain/src/app/ActividadListaTarea/ListaTarea.tsx
@@ -1,25 +1,71 @@
+import Icon from '@expo/vector-icons/Fontisto';
 import { navigate } from 'expo-router/build/global-state/routing';
 import React from 'react';
-import { Button, StyleSheet, Text } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useAppContext } from '../../context/AppContextProvider';
 
 export default function ListaTarea() {
   //
-  let context = useAppContext()
+  const context = useAppContext()
   //
-  
-  //
-    return (
-    <SafeAreaView>
-      <Text>ListaTarea</Text>
-        {context.listaTareas.map((elemento,index)=>(
-            <Text key={index}>{elemento.texto} {elemento.activa ? "activo" : "Inactivo"}</Text>
-        ))}
 
-    <Button title='+' onPress={()=>{
-      navigate("/ActividadListaTarea/CrearTarea")
-    }} />
+  function checkboxTarea(index: number) {
+    let lista = [...context.listaTareas]
+    let tarea = context.listaTareas[index]
+    tarea.activa = !tarea.activa
+    context.listaTareas[index] = tarea
+    return lista
+    // const neoArray = context.listaTareas.map((elemento,index2)=>{
+    //   let auxiliar = elemento.activa;
+    //   if (index === index2) {
+    //     swap(auxiliar);
+    //   }
+    //   return elemento
+    // })
+  
+    // context.setListaTareas(neoArray);
+
+  }
+
+  function swap(miBool:boolean){
+    if (miBool === true) {
+      miBool = false;
+    }else {
+      miBool = true;
+    }
+  }
+
+  function CrearIdTarea() {
+
+  }
+  //
+  return (
+    <SafeAreaView style={{ flex: 1, borderColor: "red", borderStyle: 'solid', borderWidth: 2, padding: 0, margin: 0 }}>
+      <View style={{ flex: 1, alignItems: 'center', borderColor: "red", borderStyle: 'solid', borderWidth: 2 }}>
+        <Text>ListaTarea</Text>
+        {context.listaTareas.map((elemento, index) => (
+          <View key={index}>
+            {elemento.activa ?
+              <Icon name="checkbox-active" size={24} color="black" onPress={() => {
+                // checkboxTarea(index)
+                context.setListaTareas(checkboxTarea(index))
+              }} />
+              :
+              <Icon name="checkbox-passive" size={24} color="black" onPress={() => {
+                // checkboxTarea(index)
+                context.setListaTareas(checkboxTarea(index))
+              }} />
+            }
+            <Text style={{ width: '90%', backgroundColor: "#e1e8e4" }}>{elemento.texto}</Text>
+          </View>
+        ))}
+      </View>
+      <View style={{ flex: 1, justifyContent: "flex-end", borderColor: "red", borderStyle: 'solid', borderWidth: 2 }}>
+        <Button title='+' onPress={() => {
+          navigate("/ActividadListaTarea/CrearTarea")
+        }} />
+      </View>
     </SafeAreaView>
   )
 }
--- pruebaIlain/src/app/index.tsx
+++ pruebaIlain/src/app/index.tsx
@@ -8,37 +8,13 @@ import ListaTarea from './ActividadListaTarea/ListaTarea';
 
   const index = () => {
     const router = useRouter();
-    //const {persona, setPersona} = useContext(AppContext); 
-    //const {personaActual: persona, setPersonaActual: setPersona} = useAppContext();
     let context = useAppContext();
-   // let persona = context.personaActual;
-    //let setPersona = context.setPersonaActual;
 
     return (
-      
         <View style={{flex:1}}>
           {ListaTarea()}
         </View>
     )
-  
-  /*
-          <Text>Usuario: </Text>
-          <TextInput placeholder='Usuario'/>
-              <Button title='Ir a detalleCasas' onPress={()=>{
-                  let per: Persona ={
-                    id: Math.trunc(Math.random()*1000),
-                    nombre: "Pepito",
-                    edad: 5
-                  }
-                  //agregamos al contexto
-                  context.setPersonaActual(per);
-                  router.push(
-                      {
-                          pathname: "/CrearPersona",
-                          params:{idCasa:22}
-                      }
-                  )
-              }}/>
-  */
+
   }
 export default index
